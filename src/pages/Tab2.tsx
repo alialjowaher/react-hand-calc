cacf65665be2f393d7421d4c8e106fe18b660a96
Ali AlJowaher <alinet010@gmail.com>
2020-11-22 15:54:16 +0300

    created inital app

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -8,16 +8,33 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle className="ion-text-center"> حاسبة الهـاند</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle></IonCardSubtitle>
+            <IonCardTitle></IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+          <IonGrid>
+            <IonRow className="ion-text-center">
+              <IonCol className="ion-align-self-center"><h2>لهـم</h2></IonCol>
+              <IonCol className="ion-align-self-center"><h2>الفرق</h2></IonCol>
+              <IonCol className="ion-align-self-center"><h2>لنـا</h2></IonCol>
+            </IonRow>
+            <IonRow className="ion-text-center">
+              <IonCol><h1>200</h1></IonCol>
+              <IonCol><h1>170</h1></IonCol>
+              <IonCol><h1>-30</h1></IonCol>
+            </IonRow>
+     
+      </IonGrid>
+      </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
