4bf6e58ef7efef76f6cc11a35fe8f425adf05189
Ali AlJowaher <alinet010@gmail.com>
2020-12-31 10:35:18 +0300

    fixed radialchart imports

--- src/components/RadialChart.js
+++ src/components/RadialChart.js
@@ -1,6 +1,4 @@
-import React, {
-    Component
-} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './RadialChart.css';
